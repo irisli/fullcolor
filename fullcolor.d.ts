ccc546e4d6275f4d8fe19397f8bb7381c5f8aba0
Iris Li <github@iris.li>
2019-07-26 15:58:12 +0300

    Add some crummy typescript definitions

---
+++ fullcolor.d.ts
@@ -0,0 +1,6 @@
+declare module 'fullcolor';
+
+declare function fullcolor(content:string, color: string): string;
+declare function fullcolor(content:string, r:number, g:number, b:number): string;
+
+export = fullcolor
